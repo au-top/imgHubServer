a50837195e9f7fe29571df4bd61308b5b430d5a3
au-top <1119134719@qq.com>
2020-11-12 13:09:58 +0800

    new

--- build/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.listImgPath = exports.publicPath = exports.imgSourcePath = void 0;
-const publicPath = `${__dirname}/public`;
-exports.publicPath = publicPath;
-const imgSourcePath = `${publicPath}/img`;
-exports.imgSourcePath = imgSourcePath;
-const listImgPath = `${publicPath}/listImg`;
-exports.listImgPath = listImgPath;
